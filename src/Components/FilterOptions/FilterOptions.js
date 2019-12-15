feb5d4caa4a331fd18e462bd2388b144567c7f63
klguenth <klguenth@gmail.com>
2019-12-15 15:58:16 -0600

    created FilterOptions component

---
+++ src/Components/FilterOptions/FilterOptions.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './FilterOptions.css';
+
+export default class FilterOptions extends React.Component {
+    render() {
+        <div className="filterOptions">
+            <div className="filterOptions-option">
+                <label for="filter-all">
+                    <input type="radio" value="All" id="filterAll" name="filter" />
+                    All
+                </label>
+            </div>
+            <div className="filterOptions-option">
+                <label for="filter-species">
+                    <input type="radio" value="Species" id="filterSpecies" name="filter" />
+                    Species
+                </label>
+            </div>
+            <div className="filterOptions-option">
+                <label for="filter-behavior">
+                    <input type="radio" value="Behavior" id="filterBehavior" name="filter" />
+                    Behavior
+                </label>
+            </div>
+            <div className="filterOptions-option">
+                <label for="filter-location">
+                    <input type="radio" value="Location" id="filterLocation" name="filter" />
+                    Location
+                </label>
+            </div>
+            <div className="filterOptions-option">
+                <label for="filter-date">
+                    <input type="radio" value="Date" id="filterDate" name="filter" />
+                    Date
+                </label>
+            </div>
+        </div>
+    }
+}
--- src/Components/SightingList/SightingList.js
+++ src/Components/SightingList/SightingList.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import Nav from './Nav/Nav.js';
 import Sighting from './Sighting/Sighting.js';
+import FilterOptions from './FilterOptions/FilterOptions.js';
 
 export default class SightingList extends React.Component {
     render() {
         return (
-            <section className="SightingList">
-                <>
+            <div>
+                <section className="NavBar">
+                    <Nav />
+                </section>
+                <section className="SightingList">
+                    <FilterOptions />
                     <ul>
                         <Sighting />
                     </ul>
-                </>
-            </section>
+                </section>
+            </div>
         )
     }
 }
