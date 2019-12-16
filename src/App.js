add64a67af8f1551d155c6f26c787b83bb327572
klguenth <klguenth@gmail.com>
2019-12-15 18:26:55 -0600

    updated App.js and created SignUpForm component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Components/Nav/Nav.js';
+import SignUpForm from './SignUpForm/SignUpForm.js';
 import './App.css';
 
 export default class App extends React.Component {
@@ -40,6 +41,29 @@ export default class App extends React.Component {
     return (
       <main id='App'>
         <Nav />
+        <div>
+          <header className="App">
+            Wildlife Watch
+          </header>
+          <section className="mainSection">
+            <h2 className="conserveQuote">"In the end we will conserve only what we love; we will love only what we understand; we will understand only what we are taught." -Baba Dioum</h2>
+            <div className="wwSummary">
+              <h3>Get closer to the natural world.</h3>
+              <h4>Wildlife Watch provides a space to share and record wildlife sightings with the ultimate desire to foster curiousity and inspire conservation efforts around the world.</h4>
+            </div>
+            <div className="wwSummary">
+              <h3>Simplistic Sightings</h3>
+              <h4>We created a clean, stylized interface to record and view animal sightings.</h4>
+            </div>
+            <div className="wwSummary">
+              <h3>Track Species</h3>
+              <h4>Record Species, behavior, and track animal sightings over time.</h4>
+            </div>
+            <div className="wwSummary">
+              <SignUpForm />
+            </div>
+          </section>
+        </div>
       </main>
     );
   }
