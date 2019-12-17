f05a9f467683284e4eadb6cafa13441ce5f86f67
klguenth <klguenth@gmail.com>
2019-12-16 19:19:22 -0600

    add basic styling to signup form

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -9,19 +9,19 @@ export default class SignUpForm extends React.Component {
                 <form class='signup-form'>
                 <div>
                     <label for="first-name">First name</label>
-                    <input placeholder='First Name' type="text" name='first-name' id='first-name' />
+                    <input placeholder='John' type="text" name='firstName' id='first-name' />
                 </div>
                 <div>
                     <label for="last-name">Last name</label>
-                    <input type="text" name='last-name' id='last-name' placeholder='Last Name' />
+                    <input placeholder='Doe' type="text" name='lastName' id='last-name' />
                 </div>
                 <div>
                     <label for="username">Email</label>
-                    <input type="text" name='username' id='username' />
+                    <input placeholder='wildlifewatcher@email.com' type="text" name='username' id='username' />
                 </div>
                 <div>
                     <label for="password">Password</label>
-                    <input type="password" name='password' id='password' />
+                    <input placeholder='WhaLeS1!' type="password" name='password' id='password' />
                 </div>
                 <button type='submit'>Sign Up</button>
                 </form>
