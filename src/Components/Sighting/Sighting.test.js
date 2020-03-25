8da5483c420cc1f66fba2cceabe8c7b5207301bd
klguenth <klguenth@gmail.com>
2020-03-25 11:50:28 -0500

    fixed mobile landing page view

--- src/Components/Sighting/Sighting.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Sighting from './Sighting.js';
-
-describe('Sighting', () => {
-  it('should render without crashing', () => {
-  });
-});
